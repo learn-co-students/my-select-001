c3b117311e0147dc979da6a59db32d57b7473f89
codyss <cody.schwarz@gmail.com>
2015-10-13 17:10:39 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
- # your code here!
+  if collection.nil?
+    return nil
+  else
+    i = 0
+    array = []
+    while i < collection.length
+      if yield(collection[i])
+        array << collection[i]
+      end
+      i += 1
+    end
+  end
+  array
 end
