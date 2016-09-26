c59b424dbe3fdf3d1edcf6b04bf3d3edad0e4719
Peregrin <peregringaret@gmail.com>
2016-09-26 01:56:21 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
  # your code here!
+ even = Array.new
+ i = 0
+ if collection.length > 0
+   collection.each do |x|
+     if x.even?
+       even << x
+     end
+   end
+ end
+ even
 end
